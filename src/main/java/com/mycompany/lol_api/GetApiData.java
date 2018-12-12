fdf890ce50fe2213e1ca40b34419c4366144e200
Nadim Ritter <nadim.ritter@ch.ibm.com>
2018-12-12 14:28:34 +0100

    single champ page

--- src/main/java/com/mycompany/lol_api/GetApiData.java
+++ src/main/java/com/mycompany/lol_api/GetApiData.java
@@ -24,6 +24,7 @@ public class GetApiData {
 
     private List<Summoner> summonersList = new ArrayList();
     private List<Champion> championsList = new ArrayList();
+    private Champion currentChampion;
     
 
     public void getSummonersData(String name) throws MalformedURLException, IOException {
@@ -62,7 +63,7 @@ public class GetApiData {
 
     }
 
-    public void getChmapionData() throws MalformedURLException, IOException {
+    public void getChampionData() throws MalformedURLException, IOException {
 
         //http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg
         //https://developer.riotgames.com/static-data.html
@@ -119,37 +120,16 @@ public class GetApiData {
         
     }
 
-    public void displayPictures(String name) throws MalformedURLException, IOException{
+    public void setSingelChampionData(String name) throws MalformedURLException, IOException{
         
-        String urlAllChampionNames = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name + "_0.jpg";
-
-        URL obj = new URL(urlAllChampionNames);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        // optional default is GET
-        con.setRequestMethod("GET");
-
-        //add request header
-        con.setRequestProperty("User-Agent", USER_AGENT);
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + urlAllChampionNames);
-        System.out.println("Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        for(int i = 0; i < championsList.size(); i++){
+            if(championsList.get(i).getName().equals(name)){
+                currentChampion = championsList.get(i);
+            }
         }
-        in.close();
         
-        System.out.println(response.toString());
-        
-        
-        
+        System.out.println(":::::::::::::::::::::::" + currentChampion.getName());
+
     }    
     
     
@@ -170,6 +150,18 @@ public class GetApiData {
     public void setChampionsList(List<Champion> championsList) {
         this.championsList = championsList;
     }
+
+    public Champion getCurrentChampion() {
+        return currentChampion;
+    }
+
+    public void setCurrentChampion(Champion currentChampion) {
+        this.currentChampion = currentChampion;
+    }
+
+    
+    
+    
 
     
     
